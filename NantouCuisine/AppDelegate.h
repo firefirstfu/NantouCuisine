39775456d313d402d93c1d6a4ee2df2759a481c5
firefirstfu <firefirstfu@gmail.com>
2015-07-13 13:20:06 +0800

    第一次提交

---
+++ NantouCuisine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NantouCuisine
+//
+//  Created by OSX on 2015/7/11.
+//  Copyright (c) 2015年 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
